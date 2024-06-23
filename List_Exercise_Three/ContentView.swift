91a1fdee27b4c39c26f9bcf642ff454becdbf96a
Sheilar Zune <sheilarzune@gmail.com>
2024-06-23 09:55:47 +0700

    add feedbacks

--- List_Exercise_Three/ContentView.swift
+++ List_Exercise_Three/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
         
         VStack(alignment:.leading, spacing: 10){
             VStack{
-                
+                // TODO: - profile image + name view has to be inside List view as a header content
                 Image("profile_img")
                     .padding(.bottom, 10)
                 Text("Hey!")
@@ -22,9 +22,8 @@ struct ContentView: View {
                     .padding(.bottom)
             }
             .padding()
-            
-            
                 List{
+                    // TODO: - don't add list items inside VStack. Please create an object for each menu item and create an array of menu items array and use ForEach loop here
                     VStack(alignment:.leading, spacing: 25){
                         Label("Profile", systemImage: "person")
                         Label("Home Page", systemImage: "house")
